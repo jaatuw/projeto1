ab0430854270bbf136ded055ff11641a36206c48
jaatuw <hhdeamorim@gmail.com>
2025-05-21 18:02:51 -0300

    uaibank
    progresso
    
    Co-authored-by: Lucas Vinicius <lucasviniciusdacosta2006@gmail.com>

---
+++ uaibank.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct usuario // declarando a struct
+    {
+        char nome[100];
+        int idade;
+        float saldo;
+    };
+    
+int escolha;
+
+int id = 1;
+
+void menu() { 
+    
+    printf("1.Insercao de um novo usuario: \n");
+    printf("2.Insercao de varios usuarios: \n");
+    printf("3.Busca de um usuario por id: \n");
+    printf("4.Transferencias entre usuarios: \n");
+    printf("5.Remocao de um usuario por id: \n");
+
+    do 
+    {
+    scanf("%d", &escolha);
+    } while (escolha < 1 || escolha > 5);
+}
+void confereid(int *idVetor, int *qtdu) {
+    for (int i = 0; i < qtdu; i++){
+    if (idVetor[i] == 0) { 
+        id = i;
+    } else {
+        continue;
+    }
+}
+}
+
+void insereuser (struct usuario *grupo, int *idVetor, int *qtdu) {
+    scanf(" %[^,], %d, %f", grupo[id].nome, &grupo[id].idade, &grupo[id].saldo);
+    printf("%s, %d, %.2f\n", grupo[id].nome, grupo[id].idade, grupo[id].saldo);
+    idVetor[id] = id;
+    id++;
+    qtdu++;
+}
+
+
+void insereusers(struct usuario *grupo, int *idVetor, int *qtdu) {
+    printf("Quantos usuarios quer inserir?\n");
+    int quantidade;
+    scanf("%d", &quantidade);
+    for (int i = 0; i < quantidade; i++) {
+        scanf(" %[^,], %d, %f", grupo[id].nome, &grupo[id].idade, &grupo[id].saldo);
+        idVetor[id] = id;
+        id++;
+        qtdu++;
+    }
+}
+
+
+void buscauser(struct usuario *grupo, int *idVetor, int *qtdu) {
+    printf("Qual usuario quer buscar?\n");
+    int idprocurado;
+    scanf("%d", &idprocurado);
+    if (idprocurado == idVetor[idprocurado])
+    {
+         printf("%s, %d, %.2f\n", grupo[idprocurado].nome, grupo[idprocurado].idade, grupo[idprocurado].saldo);
+    }
+    
+    
+}
+int main () {
+
+    int *idVetor;
+    idVetor = (int *) calloc(10, sizeof(int));
+    int qtdu = 0;
+    struct usuario *grupo = (struct usuario*) malloc(10*sizeof(struct usuario));
+
+    do {
+    menu();
+        switch (escolha)
+        {
+        case 1:
+            insereuser(grupo, idVetor, &qtdu);
+            break;
+        case 2:
+            insereusers(grupo, idVetor, &qtdu);
+            break;
+        case 3:
+            buscauser(grupo, idVetor, &qtdu);
+            break;
+        case 4:
+           // transferencia();
+            break;
+        case 5:
+            //removeuser();
+            break;
+        default:
+            break;
+        }
+    } while (escolha > 1 || escolha < 5);
+}
